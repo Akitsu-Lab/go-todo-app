d828e65e6cb0c2df47911af62566ba8c61b26c0a
長岡聡太 <>
2024-02-25 23:04:54 +0900

    go server

--- backend/main.go
+++ backend/main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"database/sql"
-	"log"
-	"os"
-
+	"fmt"
 	"github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
+
+	"log"
+	"net/http"
+	"os"
+	"time"
 )
 
-type task struct {
+type Task struct {
 	Id     int
 	Name   string
 	Status int
@@ -17,6 +21,7 @@ type task struct {
 var db *sql.DB
 
 func main() {
+	// MySQL接続情報
 	cfg := mysql.Config{
 		User:                 os.Getenv("DBUSER"),
 		Passwd:               os.Getenv("DBPASS"),
@@ -30,6 +35,7 @@ func main() {
 	if err != nil {
 		log.Fatal("failed to open db connection", err)
 	}
+	// Ping The Database
 	err = db.Ping()
 	if err != nil {
 		log.Fatal("failed to ping db", err)
@@ -37,4 +43,27 @@ func main() {
 
 	log.Println("データベース接続完了")
 
+	// DBからデータ取得
+
+	// サーバーの起動
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	http.Handle("/", r)
+
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:8080",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
+
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "成功")
 }
